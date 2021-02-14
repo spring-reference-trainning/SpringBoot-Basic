2c9240c2ed5a7560165aab9915d4627ee40f934f
Kim Woojae <ujae@Kimui-MacBookPro.local>
2021-02-14 15:14:21 +0900

    feat:create springboot-basic repository

---
+++ src/main/java/me/kwj1270/springboot/basic/BasicApplication.java
@@ -0,0 +1,13 @@
+package me.kwj1270.springboot.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kwj1270/springboot/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kwj1270.springboot.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
